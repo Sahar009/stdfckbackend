29e9578da05fecab9f123ece87c72b7070c3f942
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2025-01-23 00:38:03 +0100

    connect DB and user model

--- models/User.js
+++ models/User.js
@@ -1,9 +1,36 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  fullName: {
+  firstName: {
     type: String,
-    required: [true, 'Please add a name'],
+    required: [true, 'Please add a first name'],
+    trim: true
+  },
+  middleName: {
+    type: String,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Please add a last name'],
+    trim: true
+  },
+  gender: {
+    type: String,
+    required: [true, 'Please specify gender'],
+    enum: ['male', 'female', 'other']
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address']
+  },
+  region: {
+    type: String,
+    required: [true, 'Please add a region']
+  },
+  zipCode: {
+    type: String,
+    required: [true, 'Please add a zip code']
   },
   email: {
     type: String,
@@ -15,10 +42,17 @@ const userSchema = new mongoose.Schema({
       'Please enter a valid email',
     ],
   },
+  phoneNumber: {
+    type: String,
+    required: [true, 'Please add a phone number'],
+    unique: true,
+    match: [/^\d{10}$/, 'Please enter a valid 10-digit phone number']
+  },
   password: {
     type: String,
     required: [true, 'Please add a password'],
-    minLength: [6, 'Password must be up to 6 characters'],
+    match: [/^\d+$/, 'Password must contain only numbers'],
+    minLength: [6, 'Password must be at least 6 digits'],
   },
   accountNumber: {
     type: String,
