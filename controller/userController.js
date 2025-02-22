7e2212cf7a07de77bb577b6e2dd4de7d726830e5
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2025-02-22 09:40:35 +0100

    jwt update

--- controller/userController.js
+++ controller/userController.js
@@ -30,7 +30,7 @@ const generateAccountNumber = async () => {
 // Generate JWT Token
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '30d',
+        expiresIn: '1h',
     });
 };
 
